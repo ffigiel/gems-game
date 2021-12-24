28deac16f0c4fa44d71f6228d72cb07631a3e06b
Filip Figiel <figiel.filip@gmail.com>
2021-12-24 02:40:43 +0000

    chore: update build config for netlify

--- vite.config.js
+++ vite.config.js
@@ -2,37 +2,31 @@ import { defineConfig } from "vite"
 import { VitePWA } from 'vite-plugin-pwa'
 import elmPlugin from "vite-plugin-elm"
 
-export default defineConfig(({ mode }) => {
-  const isDev = mode === 'development'
-  return {
-    base: isDev
-      ? '/'
-      : '/gems-game/',
-    plugins: [
-      VitePWA({
-        registerType: 'autoUpdate',
-        includeAssets: ['favicon.ico'],
-        manifest: {
-          name: "Gems",
-          short_name: "Gems",
-          display: "standalone",
-          description: "A classic mini-game",
-          background_color: '#222222',
-          icons: [{
-            src: "img/gems_icon.png",
-            sizes: "1242x1242",
-            type: "image/png",
-          }, {
-            src: "img/gems_maskable.png",
-            sizes: "1242x1242",
-            type: "image/png",
-            purpose: "maskable",
-          }],
-        }
-      }),
-      elmPlugin({
-        //debug: false,
-      }),
-    ],
-  }
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: "Gems",
+        short_name: "Gems",
+        display: "standalone",
+        description: "A classic mini-game",
+        background_color: '#222222',
+        icons: [{
+          src: "img/gems_icon.png",
+          sizes: "1242x1242",
+          type: "image/png",
+        }, {
+          src: "img/gems_maskable.png",
+          sizes: "1242x1242",
+          type: "image/png",
+          purpose: "maskable",
+        }],
+      }
+    }),
+    elmPlugin({
+      //debug: false,
+    }),
+  ],
 })
